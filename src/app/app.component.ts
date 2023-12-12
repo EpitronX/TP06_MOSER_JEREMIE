245c57df9bbe4c598ae52c52b095c1a11a2bd9ff
Jérémie <jeremie.moser@gmail.com>
2023-12-13 00:30:57 +0100

    Nettoyage du code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,41 +20,13 @@ export class AppComponent {
   products!: Product[];
 
   constructor(private apiService: ApiService) {
-
-    // this.asyncSubject.subscribe(() => {
-    //   this.apiService.GetProductsFiltered(this.filters[0]).subscribe(
-    //     (result) => {
-    //       this.products = result;
-    //       console.log("2 there ??");
-    //       console.log(result);
-    //     },
-    //     (error) => {
-    //       console.error('Error fetching products', error);
-    //     }
-    //   );
-    // });
-
   }
 
   SendSearchParams($event: any) {
-    console.log($event);
-    // console.log("1 there ??");
     this.filters = $event;
-
-    //this.asyncSubject.next(this.apiService.GetProductsFiltered(this.filters[0]));
   }
   UserIsConnected([$isUserConnected, $connectedUser]: [boolean, Client]) {
-    console.log('1 ');
-    console.log($isUserConnected);
-    console.log($connectedUser);
     this.isUserConnected = $isUserConnected;
     this.connectedUser = $connectedUser;
-    if ($isUserConnected) {
-      this.apiService.GetProducts().subscribe(($result) => {
-        //console.log('test API :');
-        //console.log($result);
-      });
-
-    }
   }
 }
