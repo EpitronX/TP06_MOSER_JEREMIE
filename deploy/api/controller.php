7bf124da4598db199186a76af6fc2cfe6818949a
Jérémie <jeremie.moser@gmail.com>
2023-12-12 19:57:11 +0100

    modification du Back-end

--- deploy/api/controller.php
+++ deploy/api/controller.php
@@ -17,93 +17,147 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 	    return $response;
 	}
 	
-	function  getSearchCalatogue (Request $request, Response $response, $args) {
-	    $filtre = $args['filtre'];
-	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
-	   
-	    if ($filtre) {
-	      $data = json_decode($flux, true); 
-	    	
-		$res = array_filter($data, function($obj) use ($filtre)
-		{ 
-		    return strpos($obj["titre"], $filtre) !== false;
+	// Define the filterJson function outside of the getSearchCatalogue function
+	function filterJson($data, $filters)
+	{
+		$filteredData = array_filter($data, function ($item) use ($filters) {
+			foreach ($filters as $key => $value) {
+				if (isset($item[$key]) && $item[$key] != $value) {
+					return false;
+				}
+			}
+			return true;
 		});
-		$response->getBody()->write(json_encode(array_values($res)));
-	    } else {
-		 $response->getBody()->write($flux);
-	    }
-
-	    return addHeaders ($response);
+		return array_values($filteredData);
 	}
-
-	// API Nécessitant un Jwt valide
-	function getCatalogue (Request $request, Response $response, $args) {
-	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
-	    $data = json_decode($flux, true); 
-	    
-	    $response->getBody()->write(json_encode($data));
-	    
-	    return addHeaders ($response);
-	}
-
-	function optionsUtilisateur (Request $request, Response $response, $args) {
-	    
-	    // Evite que le front demande une confirmation à chaque modification
-	    $response = $response->withHeader("Access-Control-Max-Age", 600);
-	    
-	    return addHeaders ($response);
-	}
-
-	// API Nécessitant un Jwt valide
-	function getUtilisateur (Request $request, Response $response, $args) {
-	    global $entityManager;
-	    
-	    $payload = getJWTToken($request);
+	
+	// Now you can use the filterJson function within the getSearchCatalogue function
+	function getSearchCatalogue(Request $request, Response $response, $args)
+	{
+		$flux = '[
+			{
+				"id": 1,
+				"name": "Potato",
+				"price": 10.99
+			},
+			{
+				"id": 2,
+				"name": "Lettuce",
+				"price": 10.99
+			},
+			{
+				"id": 3,
+				"name": "Tomato",
+				"price": 7.99
+			},
+			{
+				"id": 1,
+				"name": "Pumpkin",
+				"price": 7.99
+			},
+			{
+				"id": 2,
+				"name": "Carot",
+				"price": 4.99
+			},
+			{
+				"id": 3,
+				"name": "Cole Crops",
+				"price": 3.99
+			}
+			]';
+			
+			$filters = array(
+				'id' => $request->getParam('id'),
+				'price' => $request->getParam('price')
+			);
+			
+			// Call the function to filter the JSON data
+			$filteredResult = filterJson(json_decode($flux, true), $filters);
+			
+			// Set the appropriate content type for the response
+			$response = $response->withHeader('Content-Type', 'application/json');
+			
+			// Write the filtered result to the response body
+			$response->getBody()->write(json_encode($filteredResult));
+			
+			return addHeaders($response);
+		}
+		
+		// API Nécessitant un Jwt valide
+		function getCatalogue (Request $request, Response $response, $args) {
+			$flux = '[
+				{
+				  "id": 1,
+				  "name": "Potato",
+				  "price": 10.99
+				},
+				{
+				  "id": 2,
+				  "name": "Lettuce",
+				  "price": 10.99
+				},
+				{
+				  "id": 3,
+				  "name": "Tomato",
+				  "price": 7.99
+				},
+				{
+				  "id": 1,
+				  "name": "Pumpkin",
+				  "price": 7.99
+				},
+				{
+				  "id": 2,
+				  "name": "Carot",
+				  "price": 4.99
+				},
+				{
+				  "id": 3,
+				  "name": "Cole Crops",
+				  "price": 3.99
+				}
+			  ]';
+			
+			$response->getBody()->write($flux);
+			
+			return addHeaders ($response);
+		}
+		function optionsUtilisateur (Request $request, Response $response, $args) {
+			
+			// Evite que le front demande une confirmation à chaque modification
+			$response = $response->withHeader("Access-Control-Max-Age", 600);
+			
+			return addHeaders ($response);
+		}
+		
+		// API Nécessitant un Jwt valide
+		function getUtilisateur (Request $request, Response $response, $args) {
+			
+			$payload = getJWTToken($request);
 	    $login  = $payload->userid;
 	    
-	    $utilisateurRepository = $entityManager->getRepository('Utilisateurs');
-	    $utilisateur = $utilisateurRepository->findOneBy(array('login' => $login));
-	    if ($utilisateur) {
-		$data = array('nom' => $utilisateur->getNom(), 'prenom' => $utilisateur->getPrenom());
-		$response = addHeaders ($response);
-		$response = createJwT ($response);
-		$response->getBody()->write(json_encode($data));
-	    } else {
-		$response = $response->withStatus(404);
-	    }
-
+		$flux = '{"nom":"feur","prenom":"jean"}';
+	    
+	    $response->getBody()->write($flux);
+	    
 	    return addHeaders ($response);
 	}
 
 	// APi d'authentification générant un JWT
 	function postLogin (Request $request, Response $response, $args) {   
-	    global $entityManager;
-	    $err=false;
-	    $body = $request->getParsedBody();
-	    $login = $body ['login'] ?? "";
-	    $pass = $body ['password'] ?? "";
-
-	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
-		$err = true;
-	    }
-	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$pass))  {
-		$err=true;
-	    }
-	    if (!$err) {
-		$utilisateurRepository = $entityManager->getRepository('Utilisateurs');
-		$utilisateur = $utilisateurRepository->findOneBy(array('login' => $login, 'password' => $pass));
-		if ($utilisateur and $login == $utilisateur->getLogin() and $pass == $utilisateur->getPassword()) {
-		    $response = addHeaders ($response);
-		    $response = createJwT ($response);
-		    $data = array('nom' => $utilisateur->getNom(), 'prenom' => $utilisateur->getPrenom());
-		    $response->getBody()->write(json_encode($data));
-		} else {          
-		    $response = $response->withStatus(403);
+	    
+		$response = createJwT ($response);
+		parse_str($request->getBody()->getContents(), $requestData);
+		if	($requestData['login'] == 'emma' && $requestData['password'] == 'toto')
+		{
+			$flux = '{"nom":"feur","prenom":"jean"}';
+			$response->getBody()->write($flux );
 		}
-	    } else {
-		$response = $response->withStatus(500);
-	    }
-
-	    return addHeaders ($response);
+		else
+		{
+			$response->withStatus(401);
+		}
+		return addHeaders ($response);
 	}
 
